d6a670ef5180dfd572e38ce749b1808978ebbe59
Jingyi Lin <linjingyikayla@gmail.com>
2021-10-06 14:10:28 -0400

    advanced version with sort alphabetically

--- Unit13/Movie-App/jquery.js
+++ Unit13/Movie-App/jquery.js
@@ -1,4 +1,6 @@
 let currentId = 0;
+let movieList = [];
+let movieObj;
 $('form').on('submit', function (e) {
     e.preventDefault();
     let rating;
@@ -25,19 +27,49 @@ $('form').on('submit', function (e) {
 
     if (rating !== undefined && title !== undefined) {
         currentId++;
-        // console.log('input');
-        $('.container2').append('<div class = "movies" id =' + currentId + '></div>');
-        $('#' + currentId).append('<h1>' + title + '</h1>');
-        // console.log('add title');
-        $('#' + currentId).append('<p> Rating: ' + rating + '</p>');
-        // console.log('r');
-        $('#' + currentId).append('<button class = "remove"> Remove </button>');
-        console.log('add button');
-        $('form').trigger("reset");
+        // console.log(rating);
+        // console.log(title);
+        movieObj = {"title": title, "rating": rating, "currentId" : currentId};
+        movieList.push(movieObj);
+        // movieList.sort(function(a,b){
+        //     a.title.localeCompare(b.title);
+        // });
+        movieList.sort(function(a, b) {
+            let titleA= a.title.toUpperCase();
+            let titleB= b.title.toUpperCase();
+            if (titleA > titleB) {
+                return 1;
+            }
+            if (titleA < titleB) {
+                return -1;
+            }
+            return 0;
+        })
+
+ $('.container2').empty();
+        for (let i = 0; i < movieList.length; i++){
+            // console.log('input');
+            let name = movieList[i].title;
+            let id = movieList[i].currentId;
+            let rate = movieList[i].rating;
+            console.log(name);
+            console.log(id);
+            console.log(rate);
+            $('.container2').append('<div class = "movies" id =' + movieList[i].currentId + '></div>');
+            console.log(movieList[i].currentId);
+            $('#' + movieList[i].currentId).append('<h1>' + movieList[i].title + '</h1>');
+            // console.log('add title');
+            $('#' + movieList[i].currentId).append('<p> Rating: ' + movieList[i].rating + '</p>');
+            // console.log('r');
+            $('#' + movieList[i].currentId).append('<button class = "remove"> Remove </button>');
+            console.log('add button');
+            }
     }
 
     $('form').trigger("reset");
 })
+
+
 
 
 $('.container2').on('click', ".remove", function (e) {
