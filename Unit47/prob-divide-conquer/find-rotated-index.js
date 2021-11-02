a5c6a80f2c4d4b05fc8600a77fe3793677f64b24
Jingyi Lin <linjingyikayla@gmail.com>
2021-11-02 15:15:30 -0400

    solve find-rotated-index issue

--- Unit47/prob-divide-conquer/find-rotated-index.js
+++ Unit47/prob-divide-conquer/find-rotated-index.js
@@ -2,65 +2,73 @@ function findRotatedIndex(arr, val) {
     let leftIDX = 0;
     let rightIDX = arr.length - 1;
     let newIDX;
-    // let arrayA = [];
-    // let arrayB = [];
     if (arr[0] < arr[rightIDX]) {
-        while(leftIDX <= rightIDX) {
-            let middleIDX = Math.floor((leftIDX + rightIDX)/2);
+        while (leftIDX <= rightIDX) {
+            let middleIDX = Math.floor((leftIDX + rightIDX) / 2);
             if (arr[middleIDX] === val) {
                 return middleIDX;
-            }else if (arr[middleIDX] > val) {
+            } else if (arr[middleIDX] > val) {
                 rightIDX = middleIDX - 1;
-            }else{
+            } else {
                 leftIDX = middleIDX + 1;
             }
         }
         return -1;
-    }else{
-        while (leftIDX <= rightIDX){
-            let middleIDX = Math.floor((leftIDX + rightIDX)/2);
-            if (arr[middleIDX] > arr[middleIDX + 1]){
-                return newIDX = middleIDX;
-            }else if(arr[middleIDX] < arr[middleIDX + 1]){
-                if ( arr[middleIDX] > arr[rightIDX] && arr[middleIDX] > arr[leftIDX]) {
-                    leftIDX = middleIDX + 1;
-                }else if ( arr[middleIDX] < arr[rightIDX] && arr[middleIDX] < arr[leftIDX]){
-                    rightIDX = middleIDX - 1;
-                }
+    } else {
+        let result;
+        newIDX = findPivotPoint(arr, leftIDX, rightIDX);
+        result = binarySearch(newIDX);
+        return result; 
+}
+
+function findPivotPoint(arr, leftIDX, rightIDX) {
+    while (leftIDX <= rightIDX) {
+        let middleIDX = Math.floor((leftIDX + rightIDX) / 2);
+        if (arr[middleIDX] > arr[middleIDX + 1]) {
+            newIDX = middleIDX;
+            return newIDX;
+        } else if (arr[middleIDX] < arr[middleIDX + 1]) {
+            if (arr[middleIDX] > arr[rightIDX] && arr[middleIDX] > arr[leftIDX]) {
+                leftIDX = middleIDX + 1;
+            } else if (arr[middleIDX] < arr[rightIDX] && arr[middleIDX] < arr[leftIDX]) {
+                rightIDX = middleIDX - 1;
             }
         }
-        if (arr[newIDX] === val) {
-            return newIDX;
-        }else if (val < arr[rightIDX]) {
-            let leftIDX = newIDX + 1;
-            let rightIDX = arr.length - 1;
-            while (leftIDX <= rightIDX){
-                let middleIDX = Math.floor((rightIDX + leftIDX)/2);
-                if (arr[middleIDX] === val) {
-                    return middleIDX;
-                }else if (arr[middleIDX] > val) {
-                    rightIDX = middleIDX - 1;
-                }else{
-                    leftIDX  = middleIDX + 1;
-                }
-            } 
-        }else if (val > arr[0]) {
-            let leftIDX = 0;
-            while (leftIDX <= newIDX){
-                let middleIDX = Math.floor((leftIDX + newIDX)/2);
-                if (arr[middleIDX] === val) {
-                    return middleIDX;
-                }else if (arr[middleIDX] > val) {
-                    newIDX = middleIDX - 1;
-                }else{
-                    leftIDX = middleIDX + 1;
-                }
-            } 
-        }
-    
-        return -1;
-  
     }
+}
+
+
+function binarySearch(newIDX) {
+    if (arr[newIDX] === val) {
+        return newIDX;
+    } else if (val < arr[arr.length - 1]) {
+        let leftIDX = newIDX + 1;
+        let rightIDX = arr.length - 1;
+        while (leftIDX <= rightIDX) {
+            let middleIDX = Math.floor((rightIDX + leftIDX) / 2);
+            if (arr[middleIDX] === val) {
+                return middleIDX;
+            } else if (arr[middleIDX] > val) {
+                rightIDX = middleIDX - 1;
+            } else {
+                leftIDX = middleIDX + 1;
+            }
+        }
+    } else if (val > arr[0]) {
+        let leftIDX = 0;
+        while (leftIDX <= newIDX) {
+            let middleIDX = Math.floor((leftIDX + newIDX) / 2);
+            if (arr[middleIDX] === val) {
+                return middleIDX;
+            } else if (arr[middleIDX] > val) {
+                newIDX = middleIDX - 1;
+            } else {
+                leftIDX = middleIDX + 1;
+            }
+        }
+    }
+    return -1;
+}
 }
 
 module.exports = findRotatedIndex
