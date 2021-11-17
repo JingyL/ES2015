5fb01516e05ef7c5bedf1982ddef263fa206d2eb
Jingyi Lin <linjingyikayla@gmail.com>
2021-11-17 13:38:38 -0500

    finish exercise

---
+++ Unit21/sql-joins/queries.sql
@@ -0,0 +1,10 @@
+-- write your queries here
+SELECT * FROM owners;
+SELECT * FROM vehicles;
+SELECT * FROM vehicles v JOIN owners o on v.owner_id = o.id;
+SELECT * FROM owners o JOIN vehicles v on v.owner_id = o.id;
+SELECT o.first_name, o.last_name, COUNT(v) FROM owners o JOIN  vehicles v on v.owner_id = o.id
+GROUP BY o.first_name, o.last_name ORDER BY o.first_name;
+
+SELECT o.first_name, o.last_name, COUNT(v), ROUND(AVG(price)) AS cost FROM owners o JOIN  vehicles v on v.owner_id = o.id
+GROUP BY o.first_name, o.last_name HAVING ROUND(AVG(price))  > 10000 AND COUNT(v) >= 2  ORDER BY o.first_name DESC;
