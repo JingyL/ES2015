c2cf10c144c0a0a43168bb01d0ad0d6a13c83dc2
Jingyi Lin <linjingyikayla@gmail.com>
2022-05-20 00:32:42 -0400

    finish

---
+++ outcomes-js-tricky-parts/guessing-game.js
@@ -0,0 +1,18 @@
+function guessingGame() {
+  const number = Math.floor(Math.random() * 100);
+  let isOver = false;
+  let numGuesses = 0;
+  return function guess(num) {
+    if (isOver) return "The game is over, you already won!";
+    numGuesses++;
+    if (num === number) {
+      isOver = true;
+      const guess = numGuesses === 1 ? "guess" : "guesses";
+      return `You win! You found ${num} in ${numGuesses} ${guess}.`;
+    }
+    if (num < number) return `${num} is too low!`;
+    if (num > number) return `${num} is too high!`;
+  };
+}
+
+module.exports = { guessingGame };
